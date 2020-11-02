44b7c721154b62e9ff7d1c1b35ba69d2bf74ebbe
irynasokolova2014@gmail.com <irynasokolova2014@gmail.com>
2020-11-02 09:30:00 +0300

    fix

--- test1/src/main/java/ru/sokolova/cache/services/Cache.java
+++ test1/src/main/java/ru/sokolova/cache/services/Cache.java
@@ -6,21 +6,21 @@ import java.util.stream.Collectors;
 
 public interface Cache<K, V> {
 
-	public void put(K k, V v);
+	void put(K k, V v);
 
-	public default Optional<V> get(K k) {
+	default Optional<V> get(K k) {
 		return Optional.ofNullable(getStorage().get(k)).map(x -> x.getV());
 	};
 
-	public Map<K, Node<K, V>> getStorage();
+	Map<K, Node<K, V>> getStorage();
 
-	public default Integer getStorageSize() {
+	default Integer getStorageSize() {
 		return getStorage().size();
 	};
 
 	Integer getMaxSize();
 
-	public default String print() {
+	default String print() {
 		return getStorage().entrySet().stream().map(e -> e.getValue().toString()).collect(Collectors.joining(","));
 	};
 }
